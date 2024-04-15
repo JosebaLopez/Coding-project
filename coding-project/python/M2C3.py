6464e33d910b3100690c4443c1c30388236f19e9
JosebaLopez <152749523+JosebaLopez@users.noreply.github.com>
2024-04-15 20:46:08 +0200

    Add files via upload

---
+++ coding-project/python/M2C3.py
@@ -0,0 +1,49 @@
+#Exercise1#
+string = "cadena de valores"
+number = 12
+list = [1, "dog", True]
+boolean = True
+
+#Exercise2#
+
+index_one = string[:3]
+
+#Exercise3#
+
+list_index_one = list[0]
+
+#Exercise4#
+
+new_number = number + 10
+
+#Exercise5#
+
+list_index_two = list[2]
+
+#Exercise6#
+
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(",")
+
+#Exercise7#
+
+upper_cadena = string[:6].upper()
+new_sentence = upper_cadena + " de valores"
+
+#Exercise8#
+
+string_interp = new_sentence + f" = {number}"
+
+#Exercise9#
+
+print('"hello world"')
+
+
+#2ª parte
+
+hi_sentence = "Hola a todo el mundo"
+query_one = "Hola" in hi_sentence
+
+bye_sentence = hi_sentence.replace("Hola", "adiós")
+
+
