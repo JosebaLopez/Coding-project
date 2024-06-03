29b868b919ea0a68b8ea87999c3c982b6f1607b7
JosebaLopez <152749523+JosebaLopez@users.noreply.github.com>
2024-06-03 19:00:22 +0200

    Add files via upload

---
+++ coding-project/python/M2C4.py
@@ -0,0 +1,40 @@
+#Exercise 1
+list = ['Scenedesmus', 'Klebsormidium', 'Chlorella']
+tuple = ('Intro', 'Abstract', ['image', 'graphs'])
+float = 3.46
+integer = 3
+from decimal import Decimal
+decimal_number = Decimal(float/integer)
+dictionary_mo = {'microalgae' : list, 'bacteria' : 'Cianobacter'}
+
+#Exercise 2
+import math 
+round_up = math.ceil(float)
+
+#Exercise 3
+
+square_root = math.sqrt(float)
+
+#Exercise 4
+microalgae = dictionary_mo['microalgae']
+
+#Exercise 5
+
+abstract = tuple[1]
+
+#Exercise 6
+
+list.append('Spirulina')
+
+#Exercise 7
+
+list[0] = 'Schyzochytrium'
+
+#Exercise 8
+
+list.sort()
+
+#Exercise 9
+
+tuple = tuple + ('Biblio',)
+
